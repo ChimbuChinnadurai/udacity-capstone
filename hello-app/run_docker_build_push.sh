bdd3ffb7592fd684559b270aaf09fbe3ea2bb9c7
Chimbu Chinnadurai <chimbu.chinnadurai@waitrose.co.uk>
2020-07-12 11:27:50 +0100

    corrected the errors

--- hello-app/run_docker_build_push.sh
+++ hello-app/run_docker_build_push.sh
@@ -3,17 +3,11 @@
 ## Complete the following steps to get Docker running locally
 
 # Step 1:
-# Create dockerpath
-# appversion=<your docker ID/path>
-
-appversion=simbu1290/capstone:1.0.0
-
-# Step 2:
 # Build image and add a descriptive tag
 
-docker build -t $dockerpath .
+docker build -t simbu1290/capstone:1.0.0 .
 
-# Step 3:
+# Step 2:
 # Push image to a docker repository
 
 docker push $appversion
